77308d47f59ca148950259a0eda2a4dfcb550973
Noveled <noveled@naver.com>
2020-06-19 10:06:58 +0900

    Initial commit

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const flash = require('connect-flash');
+const helmet = require('helmet');
+const hpp = require('hpp');
+//const RedisStore = require('connect-redis')(session);
 require('dotenv').config();
 
 const index = require('./routes');
 const connect = require('./schemas');
+//add
+const logger = require('./logger');
 
 const app = express();
 //add
@@ -17,14 +22,22 @@ connect();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.set('port', process.env.PORT || 8015 || PORT);
+app.set('port', process.env.PORT || 8001 || PORT);
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'production'){
+  app.use(morgan('combined'));
+  app.use(helmet());
+  app.use(hpp());
+} else {
+  app.use(morgan('dev'));
+}
+
+//app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
+const sessionOption = {
   resave: false,
   saveUninitialized: false,
   secret: process.env.COOKIE_SECRET,
@@ -32,7 +45,12 @@ app.use(session({
     httpOnly: true,
     secure: false,
   },
-}));
+};
+if (process.env.NODE_ENV === 'production') {
+  sessionOption.proxy = true;
+  // sessionOption.cookie.secure = true;
+}
+app.use(session(sessionOption));
 app.use(flash());
 
 app.use('/', index);
@@ -40,6 +58,8 @@ app.use('/', index);
 app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
+  logger.info('hello');
+  logger.error(err.message);
   next(err);
 });
 
